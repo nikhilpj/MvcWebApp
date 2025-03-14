848b9ec69bb8f21608c4ecb4b5671ac204152b13
Nikhil Jose <nikhilpj98@gmail.com>
2025-03-14 10:31:45 +0530

    listed all categories

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
 
 namespace WebApplication1.Data
 {
@@ -7,6 +8,16 @@ namespace WebApplication1.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
         {
 
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Scifi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Fiction", DisplayOrder = 3 });
         }
     }
 }
--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651de0c2067cd78971fc4290bd6b345db4fcf5bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
